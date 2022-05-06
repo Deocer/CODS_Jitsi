f3b2f727ab29364c8879ce7e6694356e39458a4f
Deocer Morales <moralesdeocer@gmail.com>
2022-05-06 12:48:41 +0800

    First Commit

---
+++ src/pages/Api.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-page class="flex flex-center">
+    <div style="height: 90vh;">
+      <div class="fit" id="meet"></div>
+    </div>
+    <div style="margin: auto;" v-if="!meetCall" >
+      <div class="row" style="width: 40vw;margin: auto;">
+        <img src="https://i0.wp.com/fatiherikci.com/wp-content/uploads/2020/03/jitsimeet.jpg?fit=1038%2C570&ssl=1" class="fit">        
+      </div>
+      <div class="row justify-center">
+        <input v-model ="meetName" placeholder="Set your meeting name"  type="text">
+        <button @click="intomeet" >Create Jitsi Meeting</button>            
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+//https://meet.jit.si/test123
+export default defineComponent({
+  name: 'IndexPage',
+  setup () {
+    const router = useRouter();
+    const meetName = ref(null);
+    const meetCall = ref(null);
+
+    const intomeet = () => {
+      const domain = 'meet.jit.si';
+      const options = {
+          roomName: 'JitsiMeetAPIExample',
+          width: 1200,
+          height: 600,
+          parentNode: document.querySelector('#meet'),
+          lang: 'en'
+      };
+      const api = new JitsiMeetExternalAPI(domain, options);
+      meetCall.value = true;
+    }
+
+    return {
+      router,
+      intomeet,
+      meetCall,
+    }
+  }
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="row fit"  v-if="meetName">
+      <div class="q-video fit">
+        <iframe
+          v-bind:src="meetCall"
+          frameborder="0"
+          allowfullscreen
+          style="height: 90vh;"
+        />
+      </div>
+    </div>
+    <div style="margin: auto;" v-if="!meetCall" >
+      <div class="row" style="width: 40vw;margin: auto;">
+        <img src="https://i0.wp.com/fatiherikci.com/wp-content/uploads/2020/03/jitsimeet.jpg?fit=1038%2C570&ssl=1" class="fit">        
+      </div>
+      <div class="row justify-center">
+        <input v-model ="meetName" placeholder="Set your meeting name"  type="text">
+        <button @click="intomeet" >Create Jitsi Meeting</button>            
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+//https://meet.jit.si/test123
+export default defineComponent({
+  name: 'IndexPage',
+  setup () {
+    const router = useRouter();
+    const meetName = ref(null);
+    const meetCall = ref(null);
+
+    const intomeet = () => {
+        console.log('working');
+        meetCall.value = `https://meet.jit.si/${meetName.value}`
+    }
+
+    return {
+      router,
+      intomeet,
+      meetCall,
+    }
+  }
+})
+</script>
